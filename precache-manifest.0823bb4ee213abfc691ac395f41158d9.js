b46b904839bbf39170eaf2c2e2e57b9966245931
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-03-16 16:43:21 +0100

    Updates

---
+++ precache-manifest.0823bb4ee213abfc691ac395f41158d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8ab6f53100e1e90b0e4927e10c20f4",
+    "url": "/find-your-book-app/index.html"
+  },
+  {
+    "revision": "d6b832207dc4c89055f1",
+    "url": "/find-your-book-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80a417df01e8fea4331a",
+    "url": "/find-your-book-app/static/js/2.4a15af58.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/find-your-book-app/static/js/2.4a15af58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b832207dc4c89055f1",
+    "url": "/find-your-book-app/static/js/main.2ea4ec87.chunk.js"
+  },
+  {
+    "revision": "2aa98d2dcd14b989eb66",
+    "url": "/find-your-book-app/static/js/runtime-main.34ca7b33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/find-your-book-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
